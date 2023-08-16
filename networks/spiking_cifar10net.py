e8199b9993e8d191afd1bbaaf6e6ac70a2ffbdfc
chenjallen <141482174+chenjallen@users.noreply.github.com>
2023-08-16 20:34:33 +0800

    Add files via upload

---
+++ networks/spiking_cifar10net.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn as nn
+from spikingjelly.clock_driven import functional, layer, surrogate, neuron
+
+
+class Cifar10Net(nn.Module):
+    def __init__(self, time_steps=8, v_threshold=1.0, v_reset=0.0, tau=2.0, surrogate_function=surrogate.ATan()):
+        super().__init__()
+
+        self.T = time_steps
+
+        self.static_conv = nn.Sequential(
+            nn.Conv2d(3, 256, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+        )
+
+        self.conv = nn.Sequential(
+            neuron.LIFNode(v_threshold=v_threshold, v_reset=v_reset, tau=tau, surrogate_function=surrogate_function,
+                           detach_reset=True),
+
+            nn.Conv2d(256, 256, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            neuron.LIFNode(v_threshold=v_threshold, v_reset=v_reset, tau=tau, surrogate_function=surrogate_function,
+                           detach_reset=True),
+
+            nn.Conv2d(256, 256, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            neuron.LIFNode(v_threshold=v_threshold, v_reset=v_reset, tau=tau, surrogate_function=surrogate_function,
+                           detach_reset=True),
+
+            nn.MaxPool2d(2, 2),  # 16 * 16
+
+            nn.Conv2d(256, 256, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            neuron.LIFNode(v_threshold=v_threshold, v_reset=v_reset, tau=tau, surrogate_function=surrogate_function,
+                           detach_reset=True),
+
+            nn.Conv2d(256, 256, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            neuron.LIFNode(v_threshold=v_threshold, v_reset=v_reset, tau=tau, surrogate_function=surrogate_function,
+                           detach_reset=True),
+
+            nn.Conv2d(256, 256, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            neuron.LIFNode(v_threshold=v_threshold, v_reset=v_reset, tau=tau, surrogate_function=surrogate_function,
+                           detach_reset=True),
+
+            nn.MaxPool2d(2, 2)  # 8 * 8
+        )
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            layer.Dropout(0.5),
+
+            nn.Linear(256 * 8 * 8, 128 * 4 * 4, bias=False),
+            neuron.LIFNode(v_threshold=v_threshold, v_reset=v_reset, tau=tau, surrogate_function=surrogate_function,
+                           detach_reset=True),
+
+            nn.Linear(128 * 4 * 4, 100, bias=False),
+            neuron.LIFNode(v_threshold=v_threshold, v_reset=v_reset, tau=tau, surrogate_function=surrogate_function,
+                           detach_reset=True)
+        )
+        self.boost = nn.AvgPool1d(10, 10)
+
+    def forward(self, x):
+        x = self.static_conv(x)
+        out_spikes_counter = self.boost(self.fc(self.conv(x)).unsqueeze(1)).squeeze(1)
+        for _ in range(1, self.T):
+            out_spikes_counter += self.boost(self.fc(self.conv(x)).unsqueeze(1)).squeeze(1)
+
+        return out_spikes_counter / self.T
